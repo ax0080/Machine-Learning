8c22f6a6eb1d704f75077e5e6cc7c4b501339ada
謝旻言 <78300584+ax0080@users.noreply.github.com>
2023-10-01 14:19:17 +0800

    Add files via upload
    add Newton's Method

--- HW/HW1/Linear_Regression.cpp
+++ HW/HW1/Linear_Regression.cpp
@@ -6,6 +6,8 @@
 #include <sstream>
 #include <string>
 #include <math.h>
+#include <algorithm>
+#include <random>
 
 using namespace std;
 
@@ -14,10 +16,13 @@ using namespace std;
 vector <double> infile;
 vector <double> x;
 vector <double> b;
-vector <double> y_cal;
+vector <double> ATb;
 vector <double> LU_parameter;
 vector <double> coeff;
-
+vector <double> coeff_1;
+vector <double> coeff_temp1;
+vector <double> coeff_temp2;
+vector <double> coeff_temp3;
 
 void read_file();
 void matrix_transpose(double **matrix1, double **matrix2, int n);//matrix2 = matrix1_T
@@ -25,10 +30,12 @@ void matrix_mul(double **matrix1, double **matrix2, double **matrix3, int n); //
 void matrix_LU(double **matrix1, double **matrix2, double **matrix3, int n); //matrix1 = matrix3 * matrix2
 void matrix_copy(double **matrix1, double **matrix2, int n);    //Copy matrix1 to matrix2
 void matrix_reset(double **matrix, int n);
+void vector_minus(vector <double> &vec1, vector <double> &vec2, vector <double> &vec3, int n); //vec1 - vec2 = vec3
+void matrix_mul_vector(double **matrix, vector <double> &vec1, vector <double> &vec2, int n, int size); //matrix*vec1=vec2, matrix is n*size
 
 int main()
 {
-    //**************************1.For File Input*************************//
+    //**************************0.For File Input*************************//
     read_file();
 
     for (int i = 0; i < infile.size(); i++) {
@@ -45,7 +52,7 @@ int main()
     printf("Lambda : ");
     scanf("%d", &lambda);
 
-    //**************************2.Dynamic Array*************************//
+    //**************************1.Dynamic Array*************************//
     //original A
     double **matrix = new double*[x.size()];
     for(int i = 0; i < x.size(); i++)
@@ -59,6 +66,7 @@ int main()
         }
     }
 
+    /*
     printf("Print A\n");
     for(int i=0; i < x.size(); i++)
     {
@@ -68,7 +76,8 @@ int main()
         }
         printf("\n");
     }
-    
+    */
+
     //A_T
     double **matrix_T = new double*[n_poly];
     for(int i = 0; i < n_poly; i++)
@@ -82,6 +91,7 @@ int main()
         }
     }
 
+    /*
     printf("Print AT\n");
     for(int i=0; i < n_poly; i++)
     {
@@ -91,6 +101,7 @@ int main()
         }
         printf("\n");
     }
+    */
 
     //Dynamic Array Temp1
     double **matrix_temp1 = new double*[n_poly];
@@ -107,7 +118,17 @@ int main()
     for(int i = 0; i < n_poly; i++)
         matrix_temp3[i] = new double[n_poly];
 
-    //**************************3.closed_form_LSE*************************//
+    //Dynamic Array ATA
+    double **matrix_ATA = new double*[n_poly];
+    for(int i = 0; i < n_poly; i++)
+        matrix_ATA[i] = new double[n_poly];
+
+    //Dynamic Array ATA_inv
+    double **matrix_ATA_inv = new double*[n_poly];
+    for(int i = 0; i < n_poly; i++)
+        matrix_ATA_inv[i] = new double[n_poly];
+    
+    //**************************2.Matrix Calculation*************************//
     //matrix_temp1 = ATA
     for(int i=0; i<n_poly; i++)
     {
@@ -119,6 +140,9 @@ int main()
         }
     }
     
+    matrix_copy(matrix_temp1, matrix_ATA, n_poly); 
+
+    /*
     printf("ATA\n");
     for(int i=0; i < n_poly; i++)
     {
@@ -128,6 +152,7 @@ int main()
         }
         printf("\n");
     }
+    */
 
     //matrix_temp1 is ATA+lambda*I
     for(int i=0; i<n_poly; i++)
@@ -138,27 +163,6 @@ int main()
 
     //LU Decomposition, matrix_temp2 is U and matrix_temp3 is L
     matrix_LU(matrix_temp1, matrix_temp2, matrix_temp3, n_poly);
-
-    
-    printf("Print L\n");
-    for(int i=0; i < n_poly; i++)
-    {
-        for(int j=0; j < n_poly; j++)
-        {
-            printf("%lf ", matrix_temp3[i][j]);
-        }
-        printf("\n");
-    }
-    
-    printf("Print U\n");
-    for(int i=0; i < n_poly; i++)
-    {
-        for(int j=0; j < n_poly; j++)
-        {
-            printf("%lf ", matrix_temp2[i][j]);
-        }
-        printf("\n");
-    }
 
     //matrix_temp1 = U-1
     double s;
@@ -175,15 +179,6 @@ int main()
 		}
 	}
 
-    printf("Print U-1\n");
-    for(int i=0; i < n_poly; i++)
-    {
-        for(int j=0; j < n_poly; j++)
-        {
-            printf("%lf ", matrix_temp1[i][j]);
-        }
-        printf("\n");
-    }
 
     //matrix_temp2 = L-1
     matrix_reset(matrix_temp2, n_poly);
@@ -197,59 +192,51 @@ int main()
 		}
 	}
 
-    printf("Print L-1\n");
-    for(int i=0; i < n_poly; i++)
-    {
-        for(int j=0; j < n_poly; j++)
-        {
-            printf("%lf ", matrix_temp2[i][j]);
-        }
-        printf("\n");
-    }
 
     //matrix_temp1 = U-1, matrix_temp2 = L-1, matrix_temp3 = (ATA+lambda I)-1
     matrix_mul(matrix_temp1, matrix_temp2, matrix_temp3, n_poly);
 
 
-    //coeff = ATb
+    // ATb
     for(int i=0; i<n_poly; i++)
     {
-        y_cal.push_back(0);
+        ATb.push_back(0);
         for(int j=0; j<x.size(); j++)
         {
-            y_cal[i]+=matrix_T[i][j]*b[j];
+            ATb[i]+=matrix_T[i][j]*b[j];
         }
     }
 
-
+    /*
     printf("ATb\n");
-    for(int i=0; i<y_cal.size(); i++)
+    for(int i=0; i<ATb.size(); i++)
     {
         printf("%lf ", y_cal[i]);
     }
     printf("\n");
+    */
 
-    
-
+    //**************************3.closed_form_LSE*************************//
     /////////coeff calculation/////////
     for(int i=0; i<n_poly; i++)
     {   
         double s=0;
         for(int j=0; j<n_poly; j++)
         {
-            s += matrix_temp3[i][j] * y_cal[j];
+            s += matrix_temp3[i][j] * ATb[j];
         }
         coeff.push_back(s);
     }
 
     //Print Fitting Line
+    printf("LSE: \n");
     printf("Fitting Line: ");
     for(int i=n_poly-1; i>=0; i--)
     {
         if(i>0) printf("%lfx^%d + ", coeff[n_poly-1-i], i);
         else printf("%lf\n", coeff[n_poly-1]);
     }
-
+    
     //Print Total Error
     double Error = 0; 
     for(int i=0; i<b.size(); i++)
@@ -264,6 +251,110 @@ int main()
     }
     printf("Total Error: %lf\n", Error);
 
+    matrix_reset(matrix_temp1, n_poly);
+    matrix_reset(matrix_temp2, n_poly);
+    matrix_reset(matrix_temp3, n_poly);
+    coeff.clear();
+
+
+    //**************************4.Steepest Descent*************************//
+
+    //**************************5.Newtons Method*************************//
+    coeff.reserve(n_poly);
+    printf("\n");
+    coeff_temp1.reserve(n_poly);
+    coeff_temp2.reserve(n_poly);
+    coeff_temp3.reserve(n_poly);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution <double> dist(-100, 100);
+
+    for(int i=0; i<n_poly; i++)
+    {
+        coeff_1.push_back(dist(gen));
+        //printf("coeff = %lf\n", coeff_1[i]);
+    }
+
+    double epsilon = 100;
+    double epsilon_max = 1e-6;
+
+    int Iter = 0;
+    int maxIterations = 100;
+    
+    //LU Decomposition, matrix_temp2 is U and matrix_temp3 is L
+    matrix_LU(matrix_ATA, matrix_temp2, matrix_temp3, n_poly);
+
+    //matrix_temp1 = U-1
+    s=0;
+    matrix_reset(matrix_temp1, n_poly);
+    for (int i=0; i<n_poly; i++) //U inv
+	{
+		matrix_temp1[i][i]=1/matrix_temp2[i][i];//對角元素直接倒數
+		for (int k=i-1; k>=0; k--)
+		{
+			s=0;
+			for (int j=k+1; j<=i; j++)
+				s=s+matrix_temp2[k][j]*matrix_temp1[j][i];
+			matrix_temp1[k][i]=-s/matrix_temp2[k][k];//迭代计算，按列倒序依次得到每一个值，
+		}
+	}
+
+
+    //matrix_temp2 = L-1
+    matrix_reset(matrix_temp2, n_poly);
+	for (int i=0; i<n_poly; i++) //L inv
+	{
+		matrix_temp2[i][i]=1/matrix_temp3[i][i]; //對角元素直接倒數
+		for (int k=i+1; k<n_poly; k++)
+		{
+			for (int j=i;j<=k-1;j++)
+				matrix_temp2[k][i]=matrix_temp2[k][i]-matrix_temp3[k][j]*matrix_temp2[j][i];   //迭代计算，按列顺序依次得到每一个值
+		}
+	}
+
+
+    //matrix_temp1 = U-1, matrix_temp2 = L-1, matrix_temp3 = (ATA)-1
+    matrix_mul(matrix_temp1, matrix_temp2, matrix_temp3, n_poly);
+    matrix_copy(matrix_temp3, matrix_ATA_inv, n_poly);
+
+    while(epsilon > epsilon_max && Iter < 100)
+    {
+        //Xn = Xn-1 - (A.TA)^(-1) * (A.TA(Xn-1) - A.Tb)
+        matrix_mul_vector(matrix_ATA, coeff_1, coeff_temp2, n_poly, n_poly);
+        vector_minus(coeff_temp2, ATb, coeff_temp3, n_poly);
+        matrix_mul_vector(matrix_ATA_inv, coeff_temp3, coeff_temp1, n_poly, n_poly);
+        vector_minus(coeff_1, coeff_temp1, coeff, n_poly);
+        
+
+        //Error
+        Error = 0;
+        for(int i=0; i<b.size(); i++)
+        {
+            double sum = 0;
+            for(int j=n_poly-1; j>=0; j--)
+            {
+                sum += coeff[n_poly-1-j]*pow(x[i], j);
+                
+            }
+            Error += pow(sum-b[i], 2);
+        }
+        epsilon = Error / x.size();
+        Iter++;
+    }
+    Iter = 0;
+
+    //Print Fitting Line
+    printf("Newton's Method: \n");
+    printf("Fitting Line: ");
+    for(int i=n_poly-1; i>=0; i--)
+    {
+        if(i>0) printf("%lfx^%d + ", coeff[n_poly-1-i], i);
+        else printf("%lf\n", coeff[n_poly-1]);
+    }
+    printf("Total Error: %lf", Error);
+
+
     //**************************6.Dynamic Array Release*************************//
     for(int i = 0; i < n_poly; i++)
         delete [] matrix[i];
@@ -272,6 +363,7 @@ int main()
     for(int i = 0; i < n_poly; i++)
         delete [] matrix_temp1[i];
     delete [] matrix_temp1;
+    
 
     for(int i = 0; i < n_poly; i++)
         delete [] matrix_temp2[i];
@@ -280,6 +372,14 @@ int main()
     for(int i = 0; i < n_poly; i++)
         delete [] matrix_temp3[i];
     delete [] matrix_temp3;
+
+    for(int i = 0; i < n_poly; i++)
+        delete [] matrix_ATA[i];
+    delete [] matrix_ATA;
+
+    for(int i = 0; i < n_poly; i++)
+        delete [] matrix_ATA_inv[i];
+    delete [] matrix_ATA_inv;
 
     system("pause");
     return 0;
@@ -410,6 +510,25 @@ void matrix_reset(double **matrix, int n)
     }
 }
 
+void vector_minus(vector <double> &vec1, vector <double> &vec2, vector <double> &vec3, int n) //vec1 - vec2 = vec3
+{
+    for(int i=0; i<n; i++)
+    {
+        vec3[i] = vec1[i] - vec2[i];
+    }
+}
+
+void matrix_mul_vector(double **matrix, vector <double> &vec1, vector <double> &vec2, int n, int size) //matrix*vec1=vec2, matrix is n*size
+{
+    for(int i=0; i<n; i++)
+    {
+        vec2[i] = 0;
+        for(int j=0; j<size; j++)
+        {
+            vec2[i]+=matrix[i][j]*vec1[j];
+        }
+    }
+}
 
 /////////////////TESTING///////////////////
     //print ATA+lambda*I
